78ba1163fff4e3d2a8bab58463ad63b8f6b1ac6c
itsamelevihilton <levihilton@byui.edu>
2020-01-11 23:57:52 -0700

    changed year formatting to something better

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,4 @@
 var year = new Date();
 document.getElementById("year").innerText = year.getFullYear();
+var mod = moment(new Date(document.lastModified));
+document.getElementById("mod").innerText = mod.format('MM/DD/YYYY hh:mm:ss');
