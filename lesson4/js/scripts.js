2a6a7e8ae094136564507a94b2002c038592381c
itsamelevihilton <levihilton@byui.edu>
2020-02-01 23:55:47 -0700

    Just about done.

---
+++ lesson4/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+var year = new Date();
+var mod = moment(new Date(document.lastModified));
+document.getElementById("date").innerText = mod.format('dddd, DD MMMM YYYY');
