672d8a5986509b9f561e553bc6d76bdeb26b0462
Levi Hilton <levihilton@byui.edu>
2020-02-03 12:03:54 -0700

    Hero done. Article done. Contact info done.

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -4,3 +4,12 @@ function toggleMenu() {
 var year = new Date();
 var mod = moment(new Date(document.lastModified));
 document.getElementById("date").innerText = mod.format('dddd, DD MMMM YYYY');
+var office = {lat: 42.0970234, lng: -111.8735843};
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: office,
+    zoom: 13
+  });
+  var marker = new google.maps.Marker({position: office, map: map});
+}
