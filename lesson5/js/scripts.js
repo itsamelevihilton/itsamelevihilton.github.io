3c1a0ad2caaddebe0337b397a48fb341f30b5d96
Levi Hilton <levihilton@byui.edu>
2020-02-04 09:13:08 -0700

    removed map in smaller view.  made a few javascript optimizations.

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -4,12 +4,13 @@ function toggleMenu() {
 var currentDate = new Date();
 var dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
 "Thursday", "Friday", "Saturday"];
-document.getElementById("day1").innerText = dayNames[(currentDate.getDay() + 1) % 7];
-document.getElementById("day2").innerText = dayNames[(currentDate.getDay() + 2) % 7];
-document.getElementById("day3").innerText = dayNames[(currentDate.getDay() + 3) % 7];
-document.getElementById("day4").innerText = dayNames[(currentDate.getDay() + 4) % 7];
-document.getElementById("day5").innerText = dayNames[(currentDate.getDay() + 5) % 7];
-if (currentDate.getDay() != 5) {
+var day = new Date().getDay();
+document.getElementById("day1").innerText = dayNames[(day + 1) % 7];
+document.getElementById("day2").innerText = dayNames[(day + 2) % 7];
+document.getElementById("day3").innerText = dayNames[(day + 3) % 7];
+document.getElementById("day4").innerText = dayNames[(day + 4) % 7];
+document.getElementById("day5").innerText = dayNames[(day + 5) % 7];
+if (day != 5) {
     document.getElementById("alert").style.display = "none";
 }
 var mod = moment(new Date(document.lastModified));
