b3514b5ba4a6bb86d21c2fb6be667de591ead2bc
Levi Hilton <levihilton@byui.edu>
2020-02-24 09:20:29 -0700

    Started using the Javascript FontLoader

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ var year = new Date();
 document.getElementById("year").innerText = year.getFullYear();
 var mod = moment(new Date(document.lastModified));
 document.getElementById("mod").innerText = mod.format('MM/DD/YYYY hh:mm:ss');
+
+WebFont.load({
+    google: {
+        families: [
+            'Montserrat'
+        ]
+    }
+})
