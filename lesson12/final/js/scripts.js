b35ea4bb27820dcbd1896509ad33686e2ffb1a97
Levi Hilton <levihilton@byui.edu>
2020-04-06 06:06:28 -0600

    started final

---
+++ lesson12/final/js/scripts.js
@@ -0,0 +1,13 @@
+WebFont.load({
+    google: {
+        families: [
+            'Cabin',
+            'Raleway'
+        ]
+    }
+  });
+
+  function toggleMenu() {
+      document.getElementById("primaryNav").classList.toggle("hide");
+  }
+  
