9e5729591b7bbc059f00152369cdb34590992460
Levi Hilton <levihilton@byui.edu>
2020-04-06 19:02:42 -0600

    working on it

--- lesson12/final/js/scripts.js
+++ lesson12/final/js/scripts.js
@@ -10,4 +10,6 @@ WebFont.load({
   function toggleMenu() {
       document.getElementById("primaryNav").classList.toggle("hide");
   }
-  
+
+var mod = moment(new Date(document.lastModified));
+document.getElementById("date").innerText = mod.format('dddd, DD MMMM YYYY');
